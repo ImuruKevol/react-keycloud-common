c1b49c809a45b7a0b72b39c2538b028cd043f702
KwonTaeWook <kwon3286@esason.co.kr>
2025-02-27 16:14:07 +0900

    Update

--- index.jsx
+++ index.jsx
@@ -1,5 +1,7 @@
 import InputFile from "./src/InputFile";
 import Select from "./src/Select";
 import Switch from "./src/Switch";
+import Pagination from "./src/Pagination";
+import Modal from "./src/Modal";
 
-export { InputFile, Select, Switch };
+export { InputFile, Select, Switch, Pagination, Modal };
